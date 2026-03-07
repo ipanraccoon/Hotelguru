ec733be8418ea8a86ea26e003e12c4d9c3dd41bf
marto <marto@DESKTOP-GDSLA26>
2026-03-07 14:49:46 +0100

    sqlalchemy talán jó

--- Hotelguru/__init__.py
+++ Hotelguru/__init__.py
@@ -1,8 +1,13 @@
-"""
-The flask application package.
-"""
-
 from flask import Flask
+from Hotelguru.extensions import db
+from flask_migrate import Migrate
+from Hotelguru.config import Config
+
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+
+migrate = Migrate(app, db)
 
 import Hotelguru.views
